fb3781ba65095ebe5c2b82ceb8a77f48ba164386
Tobias Kopelke <kopelke@gmail.com>
2017-06-15 11:21:46 +0200

    ready the transports

--- lib/log-debug.ts
+++ lib/log-debug.ts
@@ -2,6 +2,6 @@
 import * as debug from 'debug'
 import { nxLogger } from 'nx-logger'
 
-export const debugTransport: nxLogger.TransportFn =
+export const transport: nxLogger.TransportFn =
   (configuration, messages) =>
     configuration.tty ? debug(configuration.namespace.join(':'))(...messages) : null
