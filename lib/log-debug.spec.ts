7630767dbc9c012ca94ab934a8b417d1f2ad6751
Tobias Kopelke <kopelke@gmail.com>
2018-01-14 22:15:57 +0100

    test:

--- lib/log-debug.spec.ts
+++ lib/log-debug.spec.ts
@@ -6,22 +6,48 @@ import * as faker from 'faker'
 
 const { setMockHandler } = debug
 
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 50
+
 describe('nx-logger-debug with tty', () => {
   beforeEach(() => {
     configure({ transport, tty: true })
   })
 
-  it('should call debug with the given parameters', done => {
+  it('should call debug with the given parameters', () => {
     const in_ns = [faker.random.word(), faker.random.word()]
     const in_msg = faker.random.word()
     const log = create(...in_ns)
+    let called = false
     setMockHandler(ns => {
       return (formatter: any, ...args: any[]) => {
         expect(ns).toEqual(in_ns.join(':'))
         expect(formatter).toEqual(in_msg)
-        done()
+        called = true
       }
     })
     log(in_msg)
+    expect(called).toBeTruthy()
+  })
+})
+
+describe('nx-logger-debug without tty', () => {
+  beforeEach(() => {
+    configure({ transport, tty: false })
+  })
+
+  it('should call debug with the given parameters', () => {
+    const in_ns = [faker.random.word(), faker.random.word()]
+    const in_msg = faker.random.word()
+    const log = create(...in_ns)
+    let called = false
+    setMockHandler(ns => {
+      return (formatter: any, ...args: any[]) => {
+        expect(ns).toEqual(in_ns.join(':'))
+        expect(formatter).toEqual(in_msg)
+        called = true
+      }
+    })
+    log(in_msg)
+    expect(called).toBeFalsy()
   })
 })
