57c171f83c120a54a4f7e06893e42b82188f2c1a
Tobias Kopelke <tobias.kopelke@liquidlabs.de>
2018-03-22 08:58:40 +0100

    chore(tests): switched some lines around to make code climate happy :-D

--- lib/log-debug.spec.ts
+++ lib/log-debug.spec.ts
@@ -1,53 +1,51 @@
 
-import { transport } from './log-debug'
-import * as debug from './__mocks__/debug'
-import { configure, create } from '@raynode/nx-logger'
 import * as faker from 'faker'
+
+import { configure, create } from '@raynode/nx-logger'
+
+import * as debug from './__mocks__/debug'
+import { transport } from './log-debug'
 
 const { setMockHandler } = debug
 
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 50
 
 describe('nx-logger-debug with tty', () => {
-  beforeEach(() => {
-    configure({ transport, tty: true })
-  })
+  beforeEach(() => configure({ transport, tty: true }))
 
   it('should call debug with the given parameters', () => {
-    const in_ns = [faker.random.word(), faker.random.word()]
-    const in_msg = faker.random.word()
-    const log = create(...in_ns)
+    const inNs = [faker.random.word(), faker.random.word()]
+    const inMsg = faker.random.word()
+    const log = create(...inNs)
     let called = false
     setMockHandler(ns => {
       return (formatter: any, ...args: any[]) => {
-        expect(ns).toEqual(in_ns.join(':'))
-        expect(formatter).toEqual(in_msg)
+        expect(ns).toEqual(inNs.join(':'))
+        expect(formatter).toEqual(inMsg)
         called = true
       }
     })
-    log(in_msg)
+    log(inMsg)
     expect(called).toBeTruthy()
   })
 })
 
 describe('nx-logger-debug without tty', () => {
-  beforeEach(() => {
-    configure({ transport, tty: false })
-  })
+  beforeEach(() => configure({ transport, tty: false }))
 
   it('should call debug with the given parameters', () => {
-    const in_ns = [faker.random.word(), faker.random.word()]
-    const in_msg = faker.random.word()
-    const log = create(...in_ns)
+    const inNs = [faker.random.word(), faker.random.word()]
+    const inMsg = faker.random.word()
+    const log = create(...inNs)
     let called = false
     setMockHandler(ns => {
       return (formatter: any, ...args: any[]) => {
-        expect(ns).toEqual(in_ns.join(':'))
-        expect(formatter).toEqual(in_msg)
+        expect(ns).toEqual(inNs.join(':'))
+        expect(formatter).toEqual(inMsg)
         called = true
       }
     })
-    log(in_msg)
+    log(inMsg)
     expect(called).toBeFalsy()
   })
 })
